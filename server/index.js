6cf9b04f889be3f8e4c3a63b6dd8375c0f80eb66
Vasan Arul <vasankarul@gmail.com>
2020-07-25 14:39:08 -0700

    React intital render completed

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const path = require('path');
 const port = 1224;
 const db = require('../db/connection.js')
 
-app.get('/', (req, res) => res.send('Financejs is a-go!'));
-
 app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('/', (req, res) => res.send('Financejs is a-go!'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
