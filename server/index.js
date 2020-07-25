919f3bd0293b4716af70c668a3cc64fc8973fcff
Vasan Arul <vasankarul@gmail.com>
2020-07-25 12:08:27 -0700

    Basic React set-up

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 1224
+const path = require('path');
+const port = 1224;
 const db = require('../db/connection.js')
 
-app.get('/', (req, res) => res.send('Financejs is a-go!'))
+app.get('/', (req, res) => res.send('Financejs is a-go!'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <div>Financing</div>
+      )
+  }
+}
+
+export default App;
